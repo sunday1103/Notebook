c1d3bd53858ce33fba3cda4d00b2b20177c7db27
YangHuang <huangyang@outlook.com>
2019-07-14 11:33:24 +0800

    整理文件夹

--- other/code/111.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String args[]) {
-        int x[] = new int[15];
-        final int y[] = x;
-        y[5] = 123;
-        System.out.println(y[5]);
-    };
